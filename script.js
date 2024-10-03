c5a01ceefd8392c458563b47c4f19be66c860335
IginlaIsrael <iginlaisraelo@gmail.com>
2024-10-03 22:00:43 +0100

    First commit

--- script.js
+++ script.js
@@ -7,30 +7,30 @@ document.addEventListener('DOMContentLoaded', function(e){
     
     let isValid = true;
     //Add message error array here
-    let message = [];
+    let messages = [];
     form.addEventListener('submit', function(event){
         event.preventDefault();
         if(username.length < 3){
             isValid = false;
             //Add error message
-            message.push("Username has to be more than 2 characters long")
+            messages.push("Username has to be more than 2 characters long")
         }
         if(!email.includes('@'&&'.')){
             isValid = false;
             //Add error message
-            message.push("email doesn`t include an @ or .");
+            messages.push("email doesn`t include an @ or .");
         }
         if(passWord.length <= 7){
             isValid = false;
             //Add error messahe here
-            message.push("Password has to be atleast 8 characters long");
+            messages.push("Password has to be atleast 8 characters long");
         }
         feedbackDiv.style.display = "block";
         if(isValid == true){
             feedbackDiv.textContent = "Registration successful!";
             feedbackDiv.style.color = "#28a745";
         } else{
-            feedbackDiv.innerHTML = message.join('<br>');
+            feedbackDiv.innerHTML = messages.join('<br>');
             feedbackDiv.style.color = "#dc3545";
         }
     })
